991b06cb361fc450f805ab5d64b3a731723c1954
Iryna Sazonenka <toffee3177@gmail.com>
2026-03-04 23:03:21 +0100

    Fix drag-and-drop issue and recipe detail styles

--- src/components/SortableRecipeCard/SortableRecipeCard.tsx
+++ src/components/SortableRecipeCard/SortableRecipeCard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import { Recipe } from '../../types/recipe.types';
@@ -9,6 +10,8 @@ interface SortableRecipeCardProps {
 }
 
 function SortableRecipeCard({ recipe }: SortableRecipeCardProps) {
+  const cardRef = useRef<HTMLDivElement>(null);
+
   const {
     attributes,
     listeners,
@@ -18,19 +21,33 @@ function SortableRecipeCard({ recipe }: SortableRecipeCardProps) {
     isDragging
   } = useSortable({ id: recipe.id });
 
+  useEffect(() => {
+    if (!isDragging && cardRef.current) {
+      cardRef.current.blur();
+    }
+  }, [isDragging]);
+
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
     opacity: isDragging ? 0.5 : 1
   };
 
+  const setRefs = (node: HTMLDivElement | null) => {
+    setNodeRef(node);
+    if (cardRef) {
+      (cardRef as React.MutableRefObject<HTMLDivElement | null>).current = node;
+    }
+  };
+
   return (
     <div
-    ref={setNodeRef}
+    ref={setRefs}
     style={style}
     className="sortable-card"
     {...attributes}
     {...listeners}
+    tabIndex={0}
     >
       <RecipeCard recipe={recipe} />
     </div>
