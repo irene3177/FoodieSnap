dfd7508bd94e8a62998ec0a397ae01483cd68d5b
Iryna Sazonenka <toffee3177@gmail.com>
2026-04-13 22:15:32 +0200

    fix: change urls

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -21,6 +21,7 @@ const generateToken = (userId: string): string => {
 };
 
 const setTokenCookie = (res: Response, token: string): void => {
+  console.log(config.cookieOptions);
   res.cookie('token', token, {
     ...config.cookieOptions
   });
